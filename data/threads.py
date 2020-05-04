cabdc4fd08be71cf1a135d3bd246b660419a43bc
Opelsine <nurbek.kenzhekhanov@astana.fizmat.kz>
2020-05-04 16:57:10 +0600

    categories

--- data/threads.py
+++ data/threads.py
@@ -3,12 +3,12 @@ from flask_login import UserMixin
 from sqlalchemy import orm
 from sqlalchemy_serializer import SerializerMixin
 
+from .categories import association_table
 from .db_session import SqlAlchemyBase
 
 
 class Thread(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'threads'
-
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
@@ -16,3 +16,6 @@ class Thread(SqlAlchemyBase, UserMixin, SerializerMixin):
     view_count = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
     comment_count = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
     user = orm.relation('User')
+    categories = orm.relation("Category",
+                              secondary=association_table,
+                              backref="threads")
