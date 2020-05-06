8c00ea130e23696610ece8ee70cd4922f68e799c
Adi Sakhantayev <asakhantayev@gmail.com>
2020-05-06 23:30:01 +0600

    requirements.txt updated

--- test_api.py
+++ test_api.py
@@ -1,5 +1,2 @@
 from requests import delete
-print(delete('http://localhost:5000/api/threads/3', json={
-                                                      'user_id': 1,
-                                                      'password': 'admin'
-                                                       }).json())
+print(delete('http://localhost:5000/api/threads/3', json={'user_id': 1, 'password': 'admin'}).json())
